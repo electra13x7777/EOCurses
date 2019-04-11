379c17892936d0416cfa160afba05a78a3caab7d
electra13x7777 <alex.f.barney@gmail.com>
2019-04-11 11:13:08 -0400

    ADD: enemies.txt - now holds data for all stratum 1 enemies| enemy.h - includes enum that holds the line that each enemy is on in enemies.txt | eocalc.c - parser now gets a specific line of data in enemies.txt

---
+++ src/enemy.h
@@ -0,0 +1,40 @@
+// File: enemy.h
+// Author: Alex Barney
+//
+// Description: Prototypes enemy structs for Etrian Odyssey 1
+
+// Struct: enemy
+//
+// Description: Stores name and relative stats for an enemy
+struct enemy
+{
+	char* name; // enemy name
+	int hp; // enemy health
+	int at; // enemy attack
+	int df; // enemy defense
+};
+
+// Enumeration: enemyval
+//
+// Description: Stores values for where enemy data is stored in enemies.txt
+enum enemyval
+{
+	Treerat_e = 1,
+	Woodfly_e = 2,
+	Mole_e = 3,
+	Clawbug_e = 4,
+	Venomfly_e = 5,
+	Hare_e = 6,
+	Fender_e = 7,
+	Mandrake_e = 8,
+	Roller_e = 9,
+	Clawfly_e = 10,
+	Warbull_e = 11,
+	Ragelope_e = 12,
+	Kuyutha_e = 13,
+	Stalker_e = 14,
+	Wolf_e = 15,
+	Skoll_e = 16,
+	Golem_e = 17,
+	Fenrir_e = 18
+};
